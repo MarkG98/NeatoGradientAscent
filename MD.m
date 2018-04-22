5d818da11eb4b621f91218bfc564ca3b5a2b68de
Mark Goldwater <mark.goldwater@students.olin.edu>
2018-04-22 19:26:28 -0400

    Improved gradient correction in script

--- MD.m
+++ MD.m
@@ -1,32 +1,44 @@
-xLevel = 0.15;
-yLevel = 0.0145;
+xLevel = 0.18;
 
 pub = rospublisher('/raw_vel');
-msg = rosmessage(pub);
+
+Fmsg = rosmessage(pub);
+Fmsg.Data = [0.07,0.07];
+
+TLmsg = rosmessage(pub);
+TLmsg.Data = [-0.05,0.05];
+
+TRmsg = rosmessage(pub);
+TRmsg.Data = [0.05,-0.05];
+
+Smsg = rosmessage(pub);
+Smsg.Data = [0,0];
+
 sub_accel = rossubscriber('/accel');
+
 pause(1)
 
-threshold = 0.045;
+threshold = 0.035;
 
 
 while(abs(sub_accel.LatestMessage.Data(1)) > xLevel)
     sub_accel.LatestMessage.Data
-    msg.Data = [0.07, 0.07];
-    send(pub,msg);
-    pause(0.01);
-    while(abs(sub_accel.LatestMessage.Data(2)) > threshold)
+    send(pub,Fmsg);
+    pause(0.1);
+    if(abs(sub_accel.LatestMessage.Data(2)) > threshold)
         yMessage = sub_accel.LatestMessage.Data(2);
 
         if yMessage > 0
-            msg.Data = [-0.05, 0.05];
-            send(pub,msg);
+            while(abs(sub_accel.LatestMessage.Data(2)) > threshold)
+            send(pub,TLmsg);
+            end
         end
 
         if yMessage < 0
-            msg.Data = [0.05, -0.05];
-            send(pub,msg);
+            while(abs(sub_accel.LatestMessage.Data(2)) > threshold)
+            send(pub,TRmsg);
+            end
         end        
     end
 end
-msg.Data = [0,0];
-send(pub,msg);
+send(pub,Smsg);
